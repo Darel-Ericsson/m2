89877f76a018cb87fbbb8a5639737e20579ec991
Cristobal <cr.inzza@gmail.com>
2023-07-28 20:08:15 -0400

    Comienzo Grupal 5

---
+++ assets/js/logica.js
@@ -0,0 +1,36 @@
+
+document.getElementById("mobile-menu").addEventListener("click", function () {
+    var navList = document.getElementById("nav-list");
+    if (navList.classList.contains("active")) {
+        navList.classList.remove("active");
+    } else {
+        navList.classList.add("active");
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var menuToggle = document.getElementById("mobile-menu");
+    var navListContainer = document.getElementById("nav-list-container");
+
+    menuToggle.addEventListener("click", function() {
+        if (navListContainer.classList.contains("active")) {
+            navListContainer.classList.remove("active");
+            navListContainer.style.display = "none";
+        } else {
+            navListContainer.classList.add("active");
+            navListContainer.style.display = "block";
+        }
+    });
+});
+
+function login_usuario() {
+    var usuario = document.getElementById("login_usuario").value;
+    var pass = document.getElementById("login_pass").value;
+  
+    if (usuario === pass && usuario.length > 0) {
+      window.open("index.html");
+    } else {
+      document.getElementById("login_usuario").value = "";
+      document.getElementById("login_pass").value = "";
+    }
+  }
