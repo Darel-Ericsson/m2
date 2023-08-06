15d8d1ad1da37660999f310c65e10bff0a93aaf8
Cristobal <cr.inzza@gmail.com>
2023-08-06 08:26:16 -0400

    añadido boton carrito de compras

--- assets/js/logica.js
+++ assets/js/logica.js
@@ -1,35 +1,93 @@
 
-document.getElementById("mobile-menu").addEventListener("click", function () {
-    var navList = document.getElementById("nav-list");
-    if (navList.classList.contains("active")) {
-        navList.classList.remove("active");
+// document.getElementById("mobile-menu").addEventListener("click", function () {
+//     var navList = document.getElementById("nav-list");
+//     if (navList.classList.contains("active")) {
+//         navList.classList.remove("active");
+//     } else {
+//         navList.classList.add("active");
+//     }
+// });
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     var menuToggle = document.getElementById("mobile-menu");
+//     var navListContainer = document.getElementById("nav-list-container");
+
+//     menuToggle.addEventListener("click", function() {
+//         if (navListContainer.classList.contains("active")) {
+//             navListContainer.classList.remove("active");
+//             navListContainer.style.display = "none";
+//         } else {
+//             navListContainer.classList.add("active");
+//             navListContainer.style.display = "block";
+//         }
+//     });
+// });
+
+function cambioValor(idCant, idBtn) {
+    elemento = document.getElementById(idCant);
+    elementoBoton = document.getElementById(idBtn);
+    if (parseInt(elemento.value) > 0) {
+        elementoBoton.disabled = false;
     } else {
-        navList.classList.add("active");
+        elementoBoton.disabled = true;
+        elementoBoton.classList.remove('btn-danger');
+        elementoBoton.classList.add('btn-primary');
+        elementoBoton.textContent = "Añadir al carro";
+        contarProductos();
+        
     }
-});
+}
 
-document.addEventListener("DOMContentLoaded", function() {
-    var menuToggle = document.getElementById("mobile-menu");
-    var navListContainer = document.getElementById("nav-list-container");
+function colorBoton(idBoton, idCant) {
+    btnPresionado = document.getElementById(idBoton)
+    elementoCantidad = document.getElementById(idCant);
+    if (btnPresionado.classList.contains('btn-primary') && parseInt(elementoCantidad.value) > 0) {
+        btnPresionado.classList.remove('btn-primary');
+        btnPresionado.classList.add('btn-danger');
+        btnPresionado.textContent = "Quitar del carro";
+    } else {
+        
+        elementoCantidad.value = 0;
+        
+        btnPresionado.classList.remove('btn-danger');
+        btnPresionado.classList.add('btn-primary');
+        btnPresionado.textContent = "Añadir al carro";
+    }
+}
 
-    menuToggle.addEventListener("click", function() {
-        if (navListContainer.classList.contains("active")) {
-            navListContainer.classList.remove("active");
-            navListContainer.style.display = "none";
-        } else {
-            navListContainer.classList.add("active");
-            navListContainer.style.display = "block";
-        }
-    });
-});
+function contarProductos() {
+    var addToCartBtn = document.getElementsByName('addToCartBtn');
+    var icono = document.getElementById('contador');
+    var cont=0;
+    for(var i=0;i<addToCartBtn.length;i++){
+        if(addToCartBtn[i].classList.contains('btn-danger'))
+            cont += 1;
+    }
+    icono.textContent = cont
+    if (cont > 0){
+        contador.style.display = "block";
+    } else {
+        contador.style.display = "none";
+    }
+}
 
-function modificarCantidad(elemento, valor) {
-    inputValue = parseInt(document.getElementById(elemento).value)
-    max = document.getElementById(elemento).max
-    min = document.getElementById(elemento).min
+function agregarBtn(idBoton, idCant){
+    colorBoton(idBoton, idCant);
+    cambioValor(idCant, idBoton);
+    contarProductos();
+}
+
+
+function modificarCantidad(elemento, valor, elementoBoton) {
+    var elementoCantidad = document.getElementById(elemento);
+    inputValue = parseInt(elementoCantidad.value)
+    max = elementoCantidad.max
+    min = elementoCantidad.min
     if ((inputValue + valor) >= min && (inputValue + valor) <= max) {
-        document.getElementById(elemento).value = inputValue + valor;
+        elementoCantidad.value = inputValue + valor;
     }
+    cambioValor(elemento, elementoBoton);
+    
 }
 
 function login_usuario() {
